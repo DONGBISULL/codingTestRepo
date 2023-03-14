e8e5965046a40091be25d646a01c7905b5f31db2
DONGBISULL <81430855+DONGBISULL@users.noreply.github.com>
2023-03-14 12:15:59 +0900

    [Bronze III] Title: 알람 시계, Time: 232 ms, Memory: 18548 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int h = in.nextInt();
+        int m = in.nextInt();
+        in.close();
+        if (m < 45) {
+            h--;
+            m = 60 - (45 - m);
+            if (h < 0) {
+                h = 23;
+            }
+        } else {
+            m = m - 45;
+        }
+        System.out.println(h + " " + m);
+    }
+}
