cf1e307d4eddb6e0229c2a51982ed9d11b7b2e75
DongBi Sull <81430855+DONGBISULL@users.noreply.github.com>
2023-03-23 20:42:53 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 15180 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,23 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String ans = br.readLine();
-        String[] arr = ans.split(" ");
-        int max = 0;
-        Double sum = 0.0;
-        for (int i = 0; i < n; i++) { // 최대 값 구하기
-            int temp = Integer.parseInt(arr[i]);
-            if (max < temp) {
-                max = temp;
-            }
+        Double sum = 0.0; // 평균위한 합
+        Double[] arr = new Double[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < arr.length; i++) { // 기본 값 세팅
+            arr[i] = Double.parseDouble(st.nextToken());
         }
+        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
-            sum += Double.parseDouble(arr[i]) / max * 100;
+            sum += arr[i] / arr[arr.length - 1] * 100;
         }
         System.out.println(sum / n);
     }
